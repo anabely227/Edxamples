3894adad70f7c24056d665d0a2526493be8d2696
anabely227@gmail.com <anabely227@gmail.com>
2020-04-16 09:19:23 +0100

    react example

--- reactexample/FontChooser.js
+++ reactexample/FontChooser.js
@@ -2,26 +2,14 @@ class FontChooser extends React.Component {
 
     constructor(props) {
 	super(props);
-
-    this.state ={hidden: 'true'};
     }
-	
-	handleclick(){
-		if (this.state.hidden == 'true'){
-			this.setState ({hidden: 'false'});
-		}
-		else{
-			this.setState ({hidden: 'true'});
-		}
-	}
+    
 
     render() {
-		
-		var myHidden = this.state.hidden;
 
 	return(
 	       <div>
-	       <input style={{hidden: myHidden}} onClick={this.handleclick.bind(this)} type="checkbox" id="boldCheckbox" hidden='true'/>
+	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
 	       <button id="decreaseButton" hidden='true'>-</button>
 	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
 	       <button id="increaseButton" hidden='true'>+</button>
